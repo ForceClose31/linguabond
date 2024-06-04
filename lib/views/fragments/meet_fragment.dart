0d6af7e30f948003998e7ddc1346d38c28e1e12e
ForceClose31 <forceclose31@gmail.com>
2024-06-04 15:41:56 +0700

    feat :meet

--- lib/views/fragments/meet_fragment.dart
+++ lib/views/fragments/meet_fragment.dart
@@ -4,46 +4,18 @@ class MeetFragment extends StatelessWidget {
   const MeetFragment({super.key});
 
   @override
-  Widget build(BuildContext context) => CustomScrollView(
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CustomScrollView(
         slivers: [
           SliverToBoxAdapter(
-              child: SizedBox(height: MediaQuery.viewPaddingOf(context).top)),
-          const SliverToBoxAdapter(child: SizedBox(height: 16.0)),
-          SliverPadding(
-            padding: const EdgeInsets.symmetric(horizontal: 16.0),
-            sliver: SliverToBoxAdapter(
-              child: BannerContainer(
-                title: const Text('10 on 1 Sessions'),
-                subtitle: const Text(
-                    'Let\'s open up to the things that matter the most '),
-                actions: [
-                  TextButton(
-                    onPressed: () {},
-                    style: TextButton.styleFrom(padding: EdgeInsets.zero),
-                    child: Text(
-                      'Meet Now',
-                      style: Theme.of(context)
-                          .textTheme
-                          .bodyLarge
-                          ?.copyWith(color: kColorBorder),
-                    ),
-                  ),
-                  const SizedBox(width: 8.0),
-                  const Icon(
-                    Icons.date_range_outlined,
-                    color: kColorBorder,
-                    size: 17.0,
-                  )
-                ],
-                image: Image.asset('assets/images/Meetup Icon.png'),
-              ),
-            ),
+            child: SizedBox(height: MediaQuery.of(context).padding.top),
           ),
           const SliverToBoxAdapter(child: SizedBox(height: 16.0)),
           SliverPadding(
             padding: const EdgeInsets.symmetric(horizontal: 16.0),
             sliver: SliverToBoxAdapter(
-                child: Text('Semua Sesi',
+                child: Text('Semua Jadwal Meet',
                     style: Theme.of(context).textTheme.headlineMedium)),
           ),
           const SliverToBoxAdapter(child: SizedBox(height: 16.0)),
@@ -51,27 +23,35 @@ class MeetFragment extends StatelessWidget {
             padding: const EdgeInsets.symmetric(horizontal: 16.0),
             sliver: SliverList.builder(
               itemBuilder: (context, index) => buildMeetList(
-                  context: context,
-                  networkImage: kDummyPictureProfileUrl,
-                  name: 'Mr. Marakopolo',
-                  topic: 'Belajar Vocabulary',
-                  date: DateTime.now(),
-                  startTime: const TimeOfDay(hour: 7, minute: 30),
-                  endTime: const TimeOfDay(hour: 8, minute: 30)),
+                context: context,
+                date: DateTime.now(),
+                time: const TimeOfDay(hour: 7, minute: 30),
+                name: 'Mr. Marakopolo',
+                topic: 'Vocabulary',
+                subtopic: 'Common Vocabulary',
+              ),
               itemCount: 1000,
             ),
           ),
         ],
-      );
+      ),
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.only(bottom: 16.0, right: 16.0),
+        child: FloatingActionButton(
+          onPressed: () {},
+          child: const Icon(Icons.add),
+        ),
+      ),
+    );
+  }
 
   Widget buildMeetList({
     required BuildContext context,
-    required String networkImage,
+    required DateTime date,
+    required TimeOfDay time,
     required String name,
     required String topic,
-    required DateTime date,
-    required TimeOfDay startTime,
-    required TimeOfDay endTime,
+    required String subtopic,
   }) =>
       Column(
         children: [
@@ -86,41 +66,6 @@ class MeetFragment extends StatelessWidget {
               child: Column(
                 children: [
                   Row(
-                    children: [
-                      IgnorePointer(
-                        child: MyFilledButton.circle(
-                          bottomBorderWidth: 2.0,
-                          onPressed: () {},
-                          image: DecorationImage(
-                            image: NetworkImage(networkImage),
-                          ),
-                        ),
-                      ),
-                      const SizedBox(width: 16.0),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            name,
-                            style: Theme.of(context)
-                                .textTheme
-                                .titleLarge!
-                                .copyWith(color: kColorBorder),
-                          ),
-                          const SizedBox(height: 4.0),
-                          Text(
-                            topic,
-                            style: Theme.of(context)
-                                .textTheme
-                                .bodyMedium!
-                                .copyWith(color: kColorBorder),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                  const SizedBox(height: 16.0),
-                  Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
                       Column(
@@ -128,10 +73,12 @@ class MeetFragment extends StatelessWidget {
                         children: [
                           Row(
                             children: [
-                              const Icon(
-                                Icons.date_range_outlined,
-                                size: 17.0,
-                                color: kColorBorder,
+                              Text(
+                                "Date           : ",
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .bodyMedium!
+                                    .copyWith(color: kColorBorder),
                               ),
                               const SizedBox(width: 8.0),
                               Text(
@@ -143,17 +90,18 @@ class MeetFragment extends StatelessWidget {
                               ),
                             ],
                           ),
-                          const SizedBox(height: 4.0),
                           Row(
                             children: [
-                              const Icon(
-                                Icons.schedule,
-                                size: 17.0,
-                                color: kColorBorder,
+                              Text(
+                                "Time           : ",
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .bodyMedium!
+                                    .copyWith(color: kColorBorder),
                               ),
                               const SizedBox(width: 8.0),
                               Text(
-                                '${startTime.toFormattedString()} - ${endTime.toFormattedString()}',
+                                time.toFormattedString(),
                                 style: Theme.of(context)
                                     .textTheme
                                     .bodyMedium!
@@ -161,16 +109,76 @@ class MeetFragment extends StatelessWidget {
                               ),
                             ],
                           ),
+                          Row(
+                            children: [
+                              Text(
+                                "Teacher    : ",
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .bodyMedium!
+                                    .copyWith(color: kColorBorder),
+                              ),
+                              const SizedBox(width: 8.0),
+                              Text(
+                                name,
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .bodyMedium!
+                                    .copyWith(color: kColorBorder),
+                              ),
+                            ],
+                          ),
+                          Row(
+                            children: [
+                              Text(
+                                "Topic          : ",
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .bodyMedium!
+                                    .copyWith(color: kColorBorder),
+                              ),
+                              const SizedBox(width: 8.0),
+                              Text(
+                                topic,
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .bodyMedium!
+                                    .copyWith(color: kColorBorder),
+                              ),
+                            ],
+                          ),
+                          Row(
+                            children: [
+                              Text(
+                                "Subtopic  : ",
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .bodyMedium!
+                                    .copyWith(color: kColorBorder),
+                              ),
+                              const SizedBox(width: 8.0),
+                              Text(
+                                subtopic,
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .bodyMedium!
+                                    .copyWith(color: kColorBorder),
+                              ),
+                            ],
+                          ),
+                          const SizedBox(height: 15.0),
+                          Row(
+                            children: [
+                              ElevatedButton(
+                                onPressed: () {},
+                                style: ElevatedButton.styleFrom(
+                                  backgroundColor: kColorBorder,
+                                ),
+                                child: const Text('Status: Publish'),
+                              ),
+                            ],
+                          ),
                         ],
-                      ),
-                      FilledButton(
-                        onPressed: () => NavigationHelper.to(SlidePageRoute(
-                            pageBuilder: (context) => const DetailMeetPage())),
-                        style: FilledButton.styleFrom(
-                          backgroundColor: kColorBorder,
-                          foregroundColor: kColorWhite,
-                        ),
-                        child: const Text('Gabung Sekarang'),
                       ),
                     ],
                   )
